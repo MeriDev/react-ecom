891035138708185fd7735c84053ef7f13b1b2cc5
miapiar <meri.lacouturiere@gmail.com>
2022-05-08 18:29:53 +0000

    basic routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/homepage.component';
 import './App.css';
+
+const Hats = () => {
+  return <h2>HATS</h2>;
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/hats" element={<Hats />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
